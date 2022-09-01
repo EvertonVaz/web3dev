50b5f3816cd0ac42bc1108bc7156be7d09c5c07b
EvertonVaz <etovaz.web@gmail.com>
2022-08-31 22:31:43 -0300

    variaveis de ambiente

--- gameNFT/hardhat.config.js
+++ gameNFT/hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/xhLUItuU6vdbVsvRoCIudgEm8RuprB_y",
-      accounts: ["2a1dceb825590ec423fc7bf13dfeeb786eac94bfb340d2e002510df76c129fbd"],
+      url: process.env.ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
